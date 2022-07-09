3899f621434602c8c4775b6b54bdf33ab64d6cea
DKatuk <dilarakatuk@gmail.com>
2022-07-09 19:55:44 +0300

    Completed assignment

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -5,6 +5,8 @@ import Comment from "./Comment";
 function BlogPost() {
   return (
     <div id="blog-post">
+       {/* BlogContent is being returned from BlogPost */}
+      {/* Therefore, BlogContent is a child of BlogPost */}
       <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
       <Comment commentText="I agree with this statement. - Angela Merkel" />
       <Comment commentText="A universal truth. - Noam Chomsky" />
